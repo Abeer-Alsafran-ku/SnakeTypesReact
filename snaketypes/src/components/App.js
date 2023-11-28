01095f30c06a97a1b86e4ee63b2dcc45ba3b61b0
Abdulwahab-Alobaid-2191115290 <abdulwahab.alobaid@sci.ku.edu.kw>
2023-11-28 11:07:10 +0300

    Revert "Login Page is working with JSON server"
    This reverts commit 888eb8ee1cd7981caa35dc05fa7dc2b380dca45f.

--- snaketypes/src/components/App.js
+++ snaketypes/src/components/App.js
@@ -5,7 +5,6 @@ import Stage from "./Stage";
 import Login from "./Login";
 import Side from "./Side";
 import MiniProfile from "./MiniProfile";
-import Registration from "./Registration";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import { useState } from 'react';
@@ -22,10 +21,8 @@ function App() {
           <Route exact path="/" Component={Home}></Route>
           {/* Stage */}
           <Route exact path="/stage" Component={Stage} />
-          {/* Login */}
+          {/* Profile */}
           <Route exact path="/login" Component={Login} />
-          {/* Registration */}
-          <Route exact path="/registration" Component={Registration} />
         </Routes>
       </div>
     </Router>
--- snaketypes/src/components/Login.js
+++ snaketypes/src/components/Login.js
@@ -4,67 +4,123 @@ import React, { useState } from "react";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  //  const [email, setEmail] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isUsernameValid, setIsUsernameValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [isConfirmPasswordValid, setIsConfirmPasswordValid] = useState(false);
 
   const handleUsernameChange = (event) => {
     const newUsername = event.target.value;
+    // Validate username (Starts with a letter, contains at least 3 letters)
+    const isValid = /^[a-zA-Z][a-zA-Z0-9]{2,}$/.test(newUsername);
     setUsername(newUsername);
+    setIsUsernameValid(isValid);
   };
+
+  /*  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+  };*/
 
   const handlePasswordChange = (event) => {
     const newPassword = event.target.value;
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{1,}$/.test(
-      newPassword
-    );
+    // Validate password (At least 1 letter, 1 number, 1 special character)
+    const isValid =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{1,}$/.test(
+        newPassword
+      );
     setPassword(newPassword);
+    setIsPasswordValid(isValid);
   };
 
-  const handeleLogin = async (event) => {
-    event.preventDefault();
+  const handleConfirmPasswordChange = (event) => {
+    const newConfirmPassword = event.target.value;
+    // Validate that confirm password matches the password
+    const isValid = newConfirmPassword === password;
+    setConfirmPassword(newConfirmPassword);
+    setIsConfirmPasswordValid(isValid);
+  };
 
-    try {
-      const response = await fetch("http://localhost:3800/users");
-      const users = await response.json();
-
-      const user = users.find(
-        (u) => u.username === username && u.password === password
-      );
-
-      if (user) {
-        console.log("You have an account");
-      } else {
-        console.log("You don't have an account");
-      }
-    } catch (error) {
-      console.error("Error fetching data from server:", error);
-    }
+  const handleSignUp = () => {
+    // Perform registration logic here
+    console.log("Registration successful!");
   };
 
   return (
     <>
-      <form onSubmit={handeleLogin}>
-        <div className="flogin">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div className="flogin">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit" className="SignIn">
-          {/*disabled={}
+      <form className="flogin">
+        <form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input type="text"></input>
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password"></input>
+          </div>
+          <button type="submit" className="SignIn">
+            {/*disabled={}
             onClick={handleSignUp}*/}
-          Log In
-        </button>
+            Log In
+          </button>
+        </form>
+
+        <br />
+        <br />
+
+        <form>
+          <div className="fregister">
+            <label>
+              Username:
+              <input
+                type="text"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+            </label>
+            {!isUsernameValid && (
+              <p>
+                Username must start with a letter and contain at least 3
+                characters.
+              </p>
+            )}
+
+            <label>
+              Password:
+              <input
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </label>
+            {!isPasswordValid && (
+              <p>
+                Password must contain at least 1 letter, 1 number, and 1 special
+                character.
+              </p>
+            )}
+
+            <label>
+              Confirm Password:
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+              />
+            </label>
+            {!isConfirmPasswordValid && <p>Passwords must match.</p>}
+
+            <button
+              disabled={
+                !isUsernameValid || !isPasswordValid || !isConfirmPasswordValid
+              }
+              onClick={handleSignUp}
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
       </form>
     </>
   );
