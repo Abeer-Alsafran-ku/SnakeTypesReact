7b1b961a88902cae07f985594ad5650fa9e700c1
Abdulwahab-Alobaid-2191115290 <abdulwahab.alobaid@sci.ku.edu.kw>
2023-11-28 11:06:45 +0300

    Revert "login(fetching from JSON server) + registration(adding to JSOM server)"
    This reverts commit cb9bbf4c6dd3395d02565fff5f8eb14033c7c0dc.

--- snaketypes/src/components/App.js
+++ snaketypes/src/components/App.js
@@ -5,6 +5,8 @@ import Stage from "./Stage";
 import Login from "./Login";
 import Side from "./Side";
 import MiniProfile from "./MiniProfile";
+import Registration from "./Registration";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import { useState } from 'react';
 
@@ -22,6 +24,8 @@ function App() {
           <Route exact path="/stage" Component={Stage} />
           {/* Login */}
           <Route exact path="/login" Component={Login} />
+          {/* Registration */}
+          <Route exact path="/registration" Component={Registration} />
         </Routes>
       </div>
     </Router>
